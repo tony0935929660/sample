fa049b1e0afacf0285dc9cc564c641b16fc84214
tony20008989 <07170237@gm.scu.edu.tw>
2023-03-04 15:15:10 +0800

    first commmit

---
+++ src/components/color/ColorSample.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="color_bar">
+    </div>
+    <br>
+    {{color}}
+</template>
+
+<script>
+export default {
+    props: {color: String},
+}
+</script>
+
+<style>
+.color_bar {
+    width: 100px;
+    height: 100px;
+    background-color: v-bind(color);
+    border-radius: 40px;
+    border: 3px solid #E0E0E0;
+}
+</style>
---
+++ src/views/SampleView.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="page__body">
+        <div class="row">
+            <div v-for="color in colorList" :key="color">
+                <colorComponent :color="color"/>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+import colorComponent from '@/components/color/ColorSample.vue'
+
+export default {
+  name: 'SampleView',
+  components: {
+    colorComponent
+  },
+  setup() {
+    const colorList = [
+        '#FFFF6F',
+        '#81C0C0',
+        '#B9B973',
+        '#C07AB8',
+        '#0066CC',
+        '#804040',
+    ]
+
+    return {
+        colorList
+    }
+  }
+}
+</script>
+
+<style>
+.page__body {
+    width: 100%;
+}
+.row {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
+</style>
